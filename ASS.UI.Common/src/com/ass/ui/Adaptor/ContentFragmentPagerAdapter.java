7f1d62fb0b11336d13ca85825acc23a37555e8c0
c6supper <c6supper@hotmail.com>
2015-04-17 18:11:02 +0800

    daily change
    Signed-off-by: c6supper <c6supper@hotmail.com>

---
+++ ASS.UI.Common/src/com/ass/ui/Adaptor/ContentFragmentPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.ass.ui.Adaptor;
+
+import java.util.List;
+
+import com.ass.ui.Fragment.ContentFragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.ListFragment;
+
+public class ContentFragmentPagerAdapter extends FragmentPagerAdapter {
+
+	private ListFragment mFragList;
+	public ContentFragmentPagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+	
+	public ContentFragmentPagerAdapter(FragmentManager fm,ListFragment list) {
+		super(fm);
+		this.mFragList = list;
+	}
+
+	@Override
+	public Fragment getItem(int arg0) {
+		
+		return ContentFragment.newInstance(mFragList.get(arg0));
+		
+	}
+
+	@Override
+	public int getCount() {
+		return mFragList.size();
+	}
+	
+	@Override
+	public CharSequence getPageTitle(int position) {
+		// TODO Auto-generated method stub
+		return mFragList.get(position).getTitle();
+	}
+
+}
